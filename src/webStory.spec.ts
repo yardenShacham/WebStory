e55045d414d7ff8d4dea6a930c8cea0ec8007e21
yarde_000 <yarden101111@gmail.com>
2017-07-17 02:42:53 +0300

    try install test env

---
+++ src/webStory.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import 'mocha';
+import {WebStory} from './webStory';
+
+describe('WebStory Test', () => {
+
+    describe('web story init test', () => {
+
+        it('should pass', () => {
+
+            expect(true).to.be.equal(true);
+        });
+
+    });
+
+});
