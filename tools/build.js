99bf09305f8775ffc3267fa7ef3ffe8b18b59934
yarde_000 <yarden101111@gmail.com>
2017-07-22 18:38:09 +0300

    updated package json for publish and set some build fixs

--- tools/build.js
+++ tools/build.js
@@ -17,36 +17,60 @@ let promise = Promise.resolve();
 promise = promise.then(() => del(['dist/*']));
 
 // Compile source code into a distributable format with Babel
+const fileCountToCreateForEachFormat = 2;
 ['es', 'cjs', 'umd'].forEach((format) => {
-  promise = promise.then(() => rollup.rollup({
-    entry: 'src/index.ts',
-    external: Object.keys(pkg.dependencies),
-    plugins: [typescript(), string({
-      include: '**/*.html'
-    }), sass({
-      output: function (styles, styleNodes) {
-        fs.writeFileSync('dist/web-story.css', styles);
-        let uglifiedCss = uglifycss.processFiles(['dist/web-story.css']);
-        fs.writeFileSync('dist/web-story.min.css', uglifiedCss);
-      }
-    })],
-  }).then(bundle => bundle.write({
-    dest: `dist/${format === 'cjs' ? 'web-story' : `web-story.${format}`}.js`,
-    format,
-    sourceMap: true,
-    moduleName: format === 'umd' ? pkg.name : undefined,
-  })));
+	promise = promise.then(() => {
+		for (let i = 0; i < fileCountToCreateForEachFormat; i++) {
+			if (i % 2 === 0) {
+				rollup.rollup({
+					entry: 'src/index.ts',
+					external: Object.keys(pkg.dependencies),
+					plugins: [typescript(), string({
+						include: '**/*.html'
+					}), sass({
+						output: function (styles, styleNodes) {
+							fs.writeFileSync('dist/web-story.css', styles);
+							let uglifiedCss = uglifycss.processFiles(['dist/web-story.css']);
+							fs.writeFileSync('dist/web-story.min.css', uglifiedCss);
+						}
+					}), uglify({}, minify)],
+				}).then(bundle => bundle.write({
+					dest: `dist/${format === 'umd' ? 'index' : `web-story.${format}.min`}.js`,
+					format,
+					sourceMap: true,
+					banner: `/* ${pkg.name} ${pkg.version} */`,
+					moduleName: format === 'umd' ? pkg.name : undefined,
+				}));
+			}
+			else {
+				rollup.rollup({
+					entry: 'src/index.ts',
+					external: Object.keys(pkg.dependencies),
+					plugins: [typescript(), string({
+						include: '**/*.html'
+					}), sass()],
+				}).then(bundle => bundle.write({
+					dest: `dist/${`web-story.${format}`}.js`,
+					format,
+					sourceMap: true,
+					banner: `/* ${pkg.name} ${pkg.version} */`,
+					moduleName: format === 'umd' ? pkg.name : undefined,
+				}))
+			}
+		}
+	});
 });
 
 // Copy package.json and LICENSE.txt
 promise = promise.then(() => {
-  delete pkg.private;
-  delete pkg.devDependencies;
-  delete pkg.scripts;
-  delete pkg.eslintConfig;
-  delete pkg.babel;
-  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-  fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
+	delete pkg.private;
+	delete pkg.devDependencies;
+	delete pkg.scripts;
+	delete pkg.eslintConfig;
+	delete pkg.babel;
+	fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+	fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
+	fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8');
 });
 
 promise.catch(err => console.error(err.stack)); // eslint-disable-line no-console
